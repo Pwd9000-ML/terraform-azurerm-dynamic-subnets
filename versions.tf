1d76a7d18a079b6271742df190d28ba180ed6077
M L <magiconion_M@hotmail.com>
2022-02-05 13:45:27 +0000

    fix versions

--- versions.tf
+++ versions.tf
@@ -1,6 +1,9 @@
 terraform {
-  required_version = "~> 1.0.0"
+  required_version = "~> 1.1.0"
   required_providers {
-    azurerm = "~> 2.62"
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 2.62.0"
+    }
   }
 }
