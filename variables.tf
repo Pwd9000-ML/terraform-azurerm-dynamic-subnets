dfbd9bb9f974fbb36f9362703666391bc5756c3e
Pwd9000-ML <pwd9000@hotmail.co.uk>
2023-07-02 12:09:57 +0100

    update vars descriptions

--- variables.tf
+++ variables.tf
@@ -7,61 +7,61 @@ variable "common_tags" {
     Terraform   = "True"
     Description = "This is a demo built with Terraform"
   }
-  description = "Optional Input - A map of key value pairs that is used to tag resources created. (Default: demo map)"
+  description = "A map of key value pairs that is used to tag resources created. (Default: demo map)"
 }
 
 variable "create_rg" {
   type        = bool
   default     = true
-  description = "Optional Input - Create a new resource group for this deployment. (Set to false to use existing resource group)"
+  description = "Create a new resource group for this deployment. (Set to false to use existing resource group)"
 }
 
 variable "create_vnet" {
   type        = bool
   default     = true
-  description = "Optional Input - Create a new Azure Virtual Network for this deployment. (Set to false to use existing Azure Virtual Network)"
+  description = "Create a new Azure Virtual Network for this deployment. (Set to false to use existing Azure Virtual Network)"
 }
 
 variable "dns_entries" {
   type        = list(string)
   default     = []
-  description = "Optional Input - Set custom dns config. If no values specified, this defaults to Azure DNS (Only in effect on newly created Vnet when variable:create_vnet=true)"
+  description = "Set custom dns config. If no values specified, this defaults to Azure DNS (Only in effect on newly created Vnet when variable:create_vnet=true)"
 }
 
 variable "environment" {
   type        = string
   default     = "Development"
-  description = "Optional Input - Value to describe the environment. Used for tagging. (Default: Development)"
+  description = "Value to describe the environment. Used for tagging. (Default: Development)"
 }
 
 variable "location" {
   type        = string
   default     = "uksouth"
-  description = "Optional Input - Location in azure where resources will be created. (Only in effect on newly created Resource Group when variable:create_rg=true)"
+  description = "Location in azure where resources will be created. (Only in effect on newly created Resource Group when variable:create_rg=true)"
 }
 
 variable "network_address_ip" {
   type        = string
   default     = "10.1.0.0"
-  description = "Optional Input - Network IP to construct network address space. (Only in effect on newly created Vnet when variable:create_vnet=true)"
+  description = "Network IP to construct network address space. (Only in effect on newly created Vnet when variable:create_vnet=true)"
 }
 
 variable "network_address_mask" {
   type        = number
   default     = 22
-  description = "Optional Input - Network address mask to construct network address space. (Only in effect on newly created Vnet when variable:create_vnet=true)"
+  description = "Network address mask to construct network address space. (Only in effect on newly created Vnet when variable:create_vnet=true)"
 }
 
 variable "virtual_network_rg_name" {
   type        = string
   default     = "Terraform-CORE-Networking"
-  description = "Optional Input - Name of the resource group the existing Vnet is in. (create_rg=false) / Name of the resource group the Vnet will be created in. (create_rg=true)."
+  description = "Name of the resource group the existing Vnet is in. (create_rg=false) / Name of the resource group the Vnet will be created in. (create_rg=true)."
 }
 
 variable "virtual_network_name" {
   type        = string
   default     = "Terraform-CORE-VNET"
-  description = "Optional Input - Name of the existing Vnet subnets will be created in. (create_vnet=false) / Name of the new Vnet that will be created. (create_vnet=true)"
+  description = "Name of the existing Vnet subnets will be created in. (create_vnet=false) / Name of the new Vnet that will be created. (create_vnet=true)"
 }
 
 variable "subnet_config" {
@@ -92,5 +92,5 @@ variable "subnet_config" {
       cidr_base = "10.1.2.0"
     }
   }
-  description = "Optional Input - Subnet config map for each subnet to be created in either existing or newly created VNET based on. (create_vnet=true/false)"
+  description = "Subnet config map for each subnet to be created in either existing or newly created VNET based on. (create_vnet=true/false)"
 }
